c9a90949b117f19e4a2ea4bcd2233aba19c65e5f
Joshua Campbell <me@joshuadcampbell.com>
2022-10-22 19:29:25 -0700

    Renaming old config and launch and adding lenovo for Polybar

---
+++ dots/polybar/lenovo-launch.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+set -x
+
+# Terminate already running bar instances
+
+  pkill polybar
+
+# Launch Polybar for each monitor
+  
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    if [ $m == 'HDMI-0' ]
+    then
+      MONITOR=$m polybar -rq -c ~/.config/polybar/config.ini right &
+    elif [ $m == 'DP-2' ]
+    then 
+      MONITOR=$m polybar -rq -c ~/.config/polybar/config.ini left &
+    else
+      MONITOR=$m polybar -rq -c ~/.config/polybar/config.ini right &
+    fi
+  done
+else
+#  polybar -rq -c ~/.config/polybar/config.ini right &
+  polybar -rq -c ~/.config/polybar/config.ini base &
+fi
