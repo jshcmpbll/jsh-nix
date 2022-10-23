1c685d2b91be12d8315028ba95d60f175d29b09d
Joshua Campbell <me@joshuadcampbell.com>
2022-10-23 14:43:26 -0700

    server-launch needs to be hard coded to the path set by home-file

--- dots/polybar/server-launch.sh
+++ dots/polybar/server-launch.sh
@@ -11,15 +11,15 @@ if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     if [ $m == 'HDMI-0' ]
     then
-      MONITOR=$m polybar -rq -c ~/git/jsh-nix/dots/polybar/config.ini left_vert &
-      # MONITOR=$m polybar -rq -c ~/.config/polybar/config.ini right &
+      MONITOR=$m polybar -rq -c /home/jsh/.config/polybar/config.ini left_vert &
+      # MONITOR=$m polybar -rq -c /home/jsh/.config/polybar/config.ini right &
     elif [ $m == 'DP-0' ]
     then 
-      MONITOR=$m polybar -rq -c ~/git/jsh-nix/dots/polybar/config.ini right &
+      MONITOR=$m polybar -rq -c /home/jsh/.config/polybar/config.ini right &
     else
-      MONITOR=$m polybar -rq -c ~/git/jsh-nix/dots/polybar/config.ini right &
+      MONITOR=$m polybar -rq -c /home/jsh/.config/polybar/config.ini right &
     fi
   done
 else
-  polybar -rq -c ~/git/jsh-nix/dots/polybar/config.ini right &
+  polybar -rq -c /home/jsh/.config/polybar/config.ini right &
 fi
