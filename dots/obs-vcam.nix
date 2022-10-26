9d7d18004d63922acab9f1d484a6f241d7c236b8
Joshua Campbell <me@joshuadcampbell.com>
2022-10-26 15:19:56 -0700

    Moving kernel module from obs vcam to lenovo config

--- dots/obs-vcam.nix
+++ dots/obs-vcam.nix
@@ -3,7 +3,7 @@
   boot = {
     extraModulePackages = with config.boot.kernelPackages;
       [ v4l2loopback.out ];
-    kernelModules = [ "v4l2loopback" "kvm-amd" ];
+    kernelModules = [ "v4l2loopback" ];
     extraModprobeConfig = ''
       options v4l2loopback video_nr=10 exclusive_caps=1 card_label="Camera"
     '';
