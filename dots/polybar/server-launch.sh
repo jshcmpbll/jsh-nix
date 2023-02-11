e9f3354192775b17f302f3c0695eee5592df1444
Joshua Campbell <me@joshuadcampbell.com>
2023-02-11 13:14:52 -0800

    Updating polybar config and getting rid of eth script

--- dots/polybar/server-launch.sh
+++ dots/polybar/server-launch.sh
@@ -9,13 +9,10 @@ set -x
   
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    if [ $m == 'HDMI-0' ]
+    if [ $m == 'DP-2' ]
     then
       MONITOR=$m polybar -rq -c /home/jsh/.config/polybar/config.ini left_vert &
       # MONITOR=$m polybar -rq -c /home/jsh/.config/polybar/config.ini right &
-    elif [ $m == 'DP-0' ]
-    then 
-      MONITOR=$m polybar -rq -c /home/jsh/.config/polybar/config.ini right &
     else
       MONITOR=$m polybar -rq -c /home/jsh/.config/polybar/config.ini right &
     fi
