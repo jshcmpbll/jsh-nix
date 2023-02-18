5344313bf6da47c16e516c4a4650d8024a629501
Joshua Campbell <me@joshuadcampbell.com>
2023-02-18 13:33:38 -0800

    Bumping stateVersion and removing from zsh config

--- dots/zsh.nix
+++ dots/zsh.nix
@@ -210,7 +210,4 @@
 
     };
   };
-
-  system.stateVersion = "22.05"; # Did you read the comment?
 }
-
--- hosts/lenovo/configuration.nix
+++ hosts/lenovo/configuration.nix
@@ -108,6 +108,6 @@
     xscreensaver.fprintAuth = true;
   };
 
-  system.stateVersion = "22.05"; # Did you read the comment?
+  system.stateVersion = "22.11"; # Did you read the comment?
 }
 
