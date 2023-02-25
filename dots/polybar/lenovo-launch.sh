a15d8e6b8c2695650c71d286552f5891b3248755
Joshua Campbell <me@joshuadcampbell.com>
2023-02-25 10:33:04 -0800

    Adding screen brightness

--- dots/polybar/lenovo-launch.sh
+++ dots/polybar/lenovo-launch.sh
@@ -11,15 +11,15 @@ if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     if [ $m == 'HDMI-0' ]
     then
-      MONITOR=$m polybar -rq -c ~/.config/polybar/config.ini right &
+      MONITOR=$m polybar -rq -c ~/.config/polybar/config.ini main &
     elif [ $m == 'DP-2' ]
     then 
       MONITOR=$m polybar -rq -c ~/.config/polybar/config.ini left &
     else
-      MONITOR=$m polybar -rq -c ~/.config/polybar/config.ini right &
+      MONITOR=$m polybar -rq -c ~/.config/polybar/config.ini main &
     fi
   done
 else
-#  polybar -rq -c ~/.config/polybar/config.ini right &
+#  polybar -rq -c ~/.config/polybar/config.ini main &
   polybar -rq -c ~/.config/polybar/config.ini base &
 fi
