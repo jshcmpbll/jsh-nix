bbc0adb159e6485a4f5b332c12264e4e08da50ed
Joshua Campbell <me@joshuadcampbell.com>
2023-03-03 21:17:36 -0800

    Adding pub keys

--- users/jsh.nix
+++ users/jsh.nix
@@ -5,6 +5,12 @@
     isNormalUser = true;
     extraGroups = [ "wheel" "sudo" "audio" "docker" ];
     shell = pkgs.zsh;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB29bu0cfW5fIIISVOycKIfEpfAekl9BDfAvea62QgfL"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINfK7f1WvpQRhhB6UFeTOY5cB5uCzHFgP1DZZMwf75WZ"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM04fFl5hQ7ALKclvE4t/StWeCr2woQxLPd0xqimKPla"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDXVBd7IcaNgtZrAPFiQKQAkTt0uFzbxYBl0qIAHSBlz"
+    ];
   };
 
   security.sudo.configFile = ''
