0ecad6159a7fe4305a48a0cb25e46d5c0ad5f545
Joshua Campbell <me@joshuadcampbell.com>
2023-03-03 16:59:29 -0800

    Adding mm config

---
+++ hosts/mm/configuration.nix
@@ -0,0 +1,138 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      ../../users/jsh.nix
+      ../../dots/zfs.nix
+      ../../dots/zsh.nix
+      ../../dots/vim.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+  };
+
+  networking = {
+    hostName = "jsh-mm"; # Define your hostname.
+    hostId = "a6bbe9e3";
+    useDHCP = true;
+    nameservers = [ "1.1.1.1" "1.0.0.1" ];
+    firewall.enable = false;
+    wg-quick.interfaces = {
+      wg0 = {
+        address = [ "10.2.0.2/32" ];
+        dns = [ "10.2.0.1" ];
+        privateKeyFile = "/persist/pvpn-canada";
+        peers = [
+          {
+            publicKey = "5YxjJV/w2nIQHnh80py03qkUIWafadRqrnG72NzfMkE=";
+            allowedIPs = [ "0.0.0.0/0" ];
+            endpoint = "37.120.205.82:51820";
+          }
+        ];
+      };
+    };
+  };
+
+  time.timeZone = "America/Los_Angeles";
+
+  security.polkit.enable = true;
+
+  virtualisation = {
+    docker.enable = true;
+    libvirtd = {
+      enable = true;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    edgetpu-compiler
+    git
+  ];
+
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers = {
+      frigate = {
+        image = "blakeblackshear/frigate:stable-amd64";
+        autoStart = true;
+        volumes = [
+          "/home/jsh/frigate/storage:/media/frigate"
+          "/home/jsh/frigate/config.yaml:/config/config.yml:ro"
+          "/etc/localtime:/etc/localtime:ro"
+        ];
+        ports = [
+          "5000:5000"
+          "1935:1935"
+        ];
+        environment = {
+          FRIGATE_RTSP_PASSWORD = "password";
+        };
+        extraOptions = [
+          "--mount=type=tmpfs,target=/tmp/cache,tmpfs-size=1000000000"
+          "--device=/dev/bus/usb:/dev/bus/usb"
+          "--device=/dev/dri/renderD128"
+          "--shm-size=64m"
+        ];
+      };
+    };
+  };
+
+  services = {
+    openssh = {
+      enable = true;
+      permitRootLogin = "no";
+      passwordAuthentication = false;
+    };
+    mosquitto = {
+      enable = true;
+      listeners = [{
+        port = 1883;
+        omitPasswordAuth = true;
+        settings = {
+          allow_anonymous = true;
+        };
+      }];
+    };
+    deluge = {
+      user = "jsh";
+      group = "users";
+      enable = true;
+      declarative = true;
+      openFirewall = true;
+      authFile = "/persist/deluge-auth";
+      config = {
+        #download_location = "/home/jsh/Downloads";
+        info_sent = 0.0;
+        prioritize_first_last_pieces = true;
+        send_info = false;
+        stop_seed_at_ratio = true;
+        stop_seed_ratio = 2;
+        outgoing_interface = "wg0";
+      };
+      web = {
+        enable = true;
+        openFirewall = true;
+      };
+    };
+    sonarr.enable = true;
+    radarr.enable = true;
+    #plex = {
+    #  enable = true;
+    #  user = "jsh";
+    #};
+  };
+
+  system.stateVersion = "22.11"; # Did you read the comment?
+}
+
