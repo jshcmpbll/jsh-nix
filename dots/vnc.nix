ca70f702c971cb97303c8278e6023f76c813ccf2
Joshua Campbell <me@joshuadcampbell.com>
2023-06-10 21:11:30 -0700

    removing multiuser.target and listen address

--- dots/vnc.nix
+++ dots/vnc.nix
@@ -3,7 +3,6 @@
   systemd.services.x11vnc = {
     enable = true;
     path = [ pkgs.gawk pkgs.nettools ];
-    wantedBy = [ "multi-user.target" ];
     requires = [ "graphical.target" ];
     after = [ "display-manager.service" ];
     description = "VNC server";
@@ -11,7 +10,7 @@
       User = "jsh";
       Group = "users";
       Type = "simple";
-      ExecStart = "${pkgs.x11vnc}/bin/x11vnc -listen 127.0.0.1 -xkb -auth /run/user/1000/gdm/Xauthority -display :1 -forever -nopw";
+      ExecStart = "${pkgs.x11vnc}/bin/x11vnc -xkb -auth /run/user/1000/gdm/Xauthority -display :1 -forever -nopw";
     };
     reloadIfChanged = true;
     restartIfChanged = true;
