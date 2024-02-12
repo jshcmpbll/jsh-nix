938455e748c99d3cd81d25bf0c1b2344e382d687
Joshua Campbell <me@joshuadcampbell.com>
2024-02-12 14:45:51 -0800

    Adding mobile key

--- users/jsh.nix
+++ users/jsh.nix
@@ -11,6 +11,8 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINfK7f1WvpQRhhB6UFeTOY5cB5uCzHFgP1DZZMwf75WZ"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM04fFl5hQ7ALKclvE4t/StWeCr2woQxLPd0xqimKPla"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDXVBd7IcaNgtZrAPFiQKQAkTt0uFzbxYBl0qIAHSBlz"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICF/mh2HQ/a/n0z5tCa/8F67unG/wYJfuj/jAFGijnYA" # Mobile
+
     ];
   };
   security.sudo.wheelNeedsPassword = false;
