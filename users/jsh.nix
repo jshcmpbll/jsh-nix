b4892eb2f540adaac7f5cfc38db3aa1e6015dad5
Joshua Campbell <me@joshuadcampbell.com>
2024-04-30 22:27:47 -0700

    new key

--- users/jsh.nix
+++ users/jsh.nix
@@ -12,7 +12,7 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM04fFl5hQ7ALKclvE4t/StWeCr2woQxLPd0xqimKPla"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDXVBd7IcaNgtZrAPFiQKQAkTt0uFzbxYBl0qIAHSBlz"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICF/mh2HQ/a/n0z5tCa/8F67unG/wYJfuj/jAFGijnYA" # Mobile
-
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICpO6ZQcBd/Hq3qJDKePcdYDEmXqQf6V7DcCXnK0tC+2" # Mobile Shortcuts
     ];
   };
   security.sudo.wheelNeedsPassword = false;
