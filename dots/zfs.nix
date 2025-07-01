bbe27e0cb3a3508e86d1ccd8e5223a3bcb2cae1b
Joshua Campbell <me@joshuadcampbell.com>
2025-07-01 13:39:33 -0700

    Override zfs snapshotting for server

--- dots/zfs.nix
+++ dots/zfs.nix
@@ -1,9 +1,8 @@
-{ config, ... }:
+{ config, lib, ... }:
 {
   boot = {
     supportedFilesystems = [ "zfs" ];
   };
-
   services = {
     zfs = {
       autoScrub.enable = true;
@@ -14,4 +13,12 @@
       };
     };
   };
+  systemd.timers.zfs-snapshot-frequent.timerConfig = if config.networking.hostName == "jsh-server" then lib.mkForce {
+    OnCalendar = "*:0,5,10,15,20,25,30,35,40,45,50,55";
+    Persistent = "yes";
+  } else lib.mkForce {
+    OnCalendar = "*:0,10,20,30,40,50";
+    Persistent = "yes";
+  };
+  services.zfs.autoSnapshot.frequent = if config.networking.hostName == "jsh-server" then 12 else 6;
 }
