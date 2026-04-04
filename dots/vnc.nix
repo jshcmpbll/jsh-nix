c510229ad057a1e27025f57f814b52fc8210b11d
Joshua Campbell <me@joshuadcampbell.com>
2026-04-04 15:23:48 -0700

    Fix x11vnc dual-monitor support

--- dots/vnc.nix
+++ dots/vnc.nix
@@ -6,11 +6,15 @@
     requires = [ "graphical.target" ];
     after = [ "display-manager.service" ];
     description = "VNC server";
+    environment = {
+      DISPLAY = ":0";
+      XAUTHORITY = "/var/run/lightdm/root/:0";
+    };
     serviceConfig = {
       User = "jsh";
       Group = "users";
       Type = "simple";
-      ExecStart = "${pkgs.x11vnc}/bin/x11vnc -listen 0.0.0.0 -display :0 -forever -shared -nopw -verbose";
+      ExecStart = "${pkgs.x11vnc}/bin/x11vnc -listen 0.0.0.0 -display :0 -auth /var/run/lightdm/root/:0 -forever -shared -nopw -verbose -xrandr -xinerama";
     };
     reloadIfChanged = true;
     restartIfChanged = true;
