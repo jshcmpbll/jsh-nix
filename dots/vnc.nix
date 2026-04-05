5b2c35328b6e509bfa3f4062de40e7c01d1436da
Joshua Campbell <me@joshuadcampbell.com>
2026-04-04 23:34:10 -0700

    Fix x11vnc auth: use user Xauthority

--- dots/vnc.nix
+++ dots/vnc.nix
@@ -8,13 +8,13 @@
     description = "VNC server";
     environment = {
       DISPLAY = ":0";
-      XAUTHORITY = "/var/run/lightdm/root/:0";
+      XAUTHORITY = "/home/jsh/.Xauthority";
     };
     serviceConfig = {
       User = "jsh";
       Group = "users";
       Type = "simple";
-      ExecStart = "${pkgs.x11vnc}/bin/x11vnc -listen 0.0.0.0 -display :0 -auth /var/run/lightdm/root/:0 -forever -shared -nopw -verbose -xrandr -xinerama";
+      ExecStart = "${pkgs.x11vnc}/bin/x11vnc -listen 0.0.0.0 -display :0 -auth /home/jsh/.Xauthority -forever -shared -nopw -verbose -xrandr -xinerama";
     };
     reloadIfChanged = true;
     restartIfChanged = true;
