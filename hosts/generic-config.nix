9737f0d785a5b86d1acb91084546b2471ea6aaf4
Joshua Campbell <me@joshuadcampbell.com>
2026-04-05 20:22:59 -0700

    Fix i3 config symlink loop on nixos-rebuild

--- hosts/generic-config.nix
+++ hosts/generic-config.nix
@@ -117,7 +117,6 @@
       windowManager.i3 = {
         enable = true;
         package = pkgs.i3-gaps;
-        configFile = "/etc/i3/config";
       };
       xkb = {
         options = "ctrl:swapcaps";
